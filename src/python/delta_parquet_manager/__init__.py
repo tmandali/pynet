5a08a26260f8cb8b570edb88c7d0f11890139ee7
timur-mandali_lcw <timur.mandali@lcwaikikiki.com>
2025-11-26 22:46:42 +0300

    refactor: Update DeltaParquetManager to improve database part reading and error handling
    - Introduced a new method `_read_database_part` to handle database queries with improved error checking for unsupported database types.
    - Removed the `_generate_range` method and streamlined the data retrieval process by directly using the updated SQL query structure.
    - Enhanced the main execution flow to utilize the new method for reading database parts, ensuring better clarity and maintainability.

--- src/python/delta_parquet_manager/__init__.py
+++ src/python/delta_parquet_manager/__init__.py
@@ -1,6 +1,6 @@
-from datetime import date
 import logging
-from typing import Any, Iterable
+from typing import Any
+from urllib.parse import urlparse
 import polars as pl
 
 class DeltaParquetManager:
@@ -13,21 +13,16 @@ class DeltaParquetManager:
             console_handler.setFormatter(formatter)
             self.logger.addHandler(console_handler)
 
-    def _generate_range(self, min_id:int, max_id:int, partition:int) -> Iterable[tuple[int, int]]:
-        bucket_size = int((max_id - min_id) / partition)
-        for i in range(partition):
-            start_id = min_id + i * bucket_size
-            end_id = max_id if i == partition - 1 else start_id + bucket_size - 1
-            yield start_id, end_id
+    def _read_database_part(self, database_type:str, db_uri:str, query:str, order_by_column:str, last_id:Any, limit:int, partition:int) -> pl.DataFrame:      
 
-    def _read_data_part(self, last_id:Any, db_uri:str, query:str, order_by_column:str, limit:int, partition:int) -> pl.DataFrame:      
+        if database_type.lower() != "mssql":
+            raise ValueError(f"Database type {database_type} is not supported")
 
         range_query = f"""           
             WITH Part AS (SELECT TOP {limit} {order_by_column} FROM ({query}) source WHERE {order_by_column} > '{last_id}' ORDER BY 1),
             Range AS (SELECT NTILE({partition}) OVER (ORDER BY {order_by_column}) as Part, {order_by_column} FROM Part)
             SELECT Part, MIN({order_by_column}) as BeginPart, MAX({order_by_column}) as EndPart FROM Range GROUP BY Part
         """
-        print(range_query)
 
         range = pl.read_database_uri(range_query, db_uri).to_dicts()
         if range is None or len(range) == 0:
@@ -37,13 +32,6 @@ class DeltaParquetManager:
             SELECT * FROM ({query}) part WHERE {order_by_column} BETWEEN '{range["BeginPart"]}' AND '{range["EndPart"]}'
             """ for range in range]
 
-        # min_id = range["min_id"]
-        # max_id = range["max_id"]        
-
-        # part_list: list[str] = [f"""
-        #     SELECT * FROM ({query}) source where {order_by_column} between '{start_id}' and '{end_id}'
-        #     """ for start_id, end_id in self._generate_range(min_id, max_id, partition)]
-                
         df = pl.read_database_uri(part_list, db_uri)
         if df.is_empty():
             return None
@@ -54,8 +42,10 @@ class DeltaParquetManager:
         if partition <= 0 or partition is None:
             partition = 1
         
+        parsed_db_uri = urlparse(db_uri)
+
         while True:
-            df = self._read_data_part(last_id, db_uri, query, order_by_column, limit, partition)
+            df = self._read_database_part(parsed_db_uri.scheme, db_uri, query, order_by_column, last_id, limit, partition)
             if df is None:
                 break
 
@@ -70,4 +60,10 @@ if __name__ == "__main__":
         order_by_column="ID")
 
     for df in db_reader:
+        # df = df.with_columns(
+        #     pl.col("RowVersion").map_elements(
+        #         lambda x: x.hex() if isinstance(x, bytes) else x,
+        #         return_dtype=pl.Utf8
+        #     ).alias("RowVersion")
+        # )
         print(df.height)
