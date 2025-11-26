cc17c85a650dc2d167ff37b6b5f7567995c93e57
timur-mandali_lcw <timur.mandali@lcwaikikiki.com>
2025-11-26 22:19:18 +0300

    refactor: Enhance _read_data_part method for improved query handling and data retrieval
    - Updated the SQL query structure to utilize NTILE for partitioning, improving data range selection.
    - Changed the last_id parameter type to Any for better flexibility in handling different data types.
    - Adjusted the method to return None for empty results and streamlined the construction of the part_list for data retrieval.

--- src/python/delta_parquet_manager/__init__.py
+++ src/python/delta_parquet_manager/__init__.py
@@ -1,5 +1,6 @@
+from datetime import date
 import logging
-from typing import Iterable
+from typing import Any, Iterable
 import polars as pl
 
 class DeltaParquetManager:
@@ -19,22 +20,29 @@ class DeltaParquetManager:
             end_id = max_id if i == partition - 1 else start_id + bucket_size - 1
             yield start_id, end_id
 
-    def _read_data_part(self, last_id:int, db_uri:str, query:str, order_by_column:str, limit:int, partition:int) -> pl.DataFrame:        
-        range_query = f"""           
-            WITH Part AS (SELECT TOP {limit} {order_by_column} FROM ({query}) source WHERE {order_by_column} > {last_id} ORDER BY 1)
-            SELECT MIN({order_by_column}) as min_id, MAX({order_by_column}) as max_id FROM Part
-        """
+    def _read_data_part(self, last_id:Any, db_uri:str, query:str, order_by_column:str, limit:int, partition:int) -> pl.DataFrame:      
 
-        df = pl.read_database_uri(range_query, db_uri)
-        if df.is_empty():
+        range_query = f"""           
+            WITH Part AS (SELECT TOP {limit} {order_by_column} FROM ({query}) source WHERE {order_by_column} > '{last_id}' ORDER BY 1),
+            Range AS (SELECT NTILE({partition}) OVER (ORDER BY {order_by_column}) as Part, {order_by_column} FROM Part)
+            SELECT Part, MIN({order_by_column}) as BeginPart, MAX({order_by_column}) as EndPart FROM Range GROUP BY Part
+        """
+        print(range_query)
+
+        range = pl.read_database_uri(range_query, db_uri).to_dicts()
+        if range is None or len(range) == 0:
             return None
 
-        min_id = df["min_id"][0]
-        max_id = df["max_id"][0]        
-
         part_list: list[str] = [f"""
-            SELECT * FROM ({query}) source where {order_by_column} between {start_id} and {end_id}
-            """ for start_id, end_id in self._generate_range(min_id, max_id, partition)]
+            SELECT * FROM ({query}) part WHERE {order_by_column} BETWEEN '{range["BeginPart"]}' AND '{range["EndPart"]}'
+            """ for range in range]
+
+        # min_id = range["min_id"]
+        # max_id = range["max_id"]        
+
+        # part_list: list[str] = [f"""
+        #     SELECT * FROM ({query}) source where {order_by_column} between '{start_id}' and '{end_id}'
+        #     """ for start_id, end_id in self._generate_range(min_id, max_id, partition)]
                 
         df = pl.read_database_uri(part_list, db_uri)
         if df.is_empty():
@@ -42,7 +50,7 @@ class DeltaParquetManager:
 
         return df                
     
-    def read_database_part(self, db_uri:str, query:str, order_by_column:str, last_id:int = 0, limit:int=1_000_000, partition:int=8):     
+    def read_database_part(self, db_uri:str, query:str, order_by_column:str, last_id:Any = 0, limit:int=1_000_000, partition:int=8):     
         if partition <= 0 or partition is None:
             partition = 1
         
