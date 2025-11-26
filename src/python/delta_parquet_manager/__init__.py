f475e5583869436d7d92d85b71088e64ef75370c
TIMUR MANDALI <TIMUR.MANDALI+lcw@Lcwaikiki.com>
2025-11-26 20:27:16 +0300

    refactor: Simplify DeltaParquetManager initialization and enhance data reading method
    - Removed parameters from the DeltaParquetManager constructor and added a new method `read_database_part` for reading data with specified parameters.
    - Updated internal methods to use parameters directly instead of instance variables, improving flexibility and clarity.
    - Adjusted the main execution block to reflect the new method structure for reading database parts.

--- src/python/delta_parquet_manager/__init__.py
+++ src/python/delta_parquet_manager/__init__.py
@@ -3,18 +3,7 @@ from typing import Iterable
 import polars as pl
 
 class DeltaParquetManager:
-    def __init__(self, db_uri:str, out_dir:str, query:str, pk_columns:str|list[str], order_by_column:str, limit:int=1_000_000, partition:int=8):
-        self.db_uri = db_uri
-        self.out_dir = out_dir
-        self.limit = limit
-        self.pk_columns = pk_columns
-        self.order_by_column = order_by_column
-        self.query = query
-
-        self.partition = 1        
-        if partition > 0:
-            self.partition = partition
-
+    def __init__(self): 
         self.logger = logging.getLogger(self.__class__.__name__)
         if not self.logger.handlers:
             # Console Handler
@@ -23,14 +12,17 @@ class DeltaParquetManager:
             console_handler.setFormatter(formatter)
             self.logger.addHandler(console_handler)
 
-    def run(self):        
+    def read_database_part(self, db_uri:str, out_dir:str, query:str, order_by_column:str, limit:int=1_000_000, partition:int=8):     
+        if partition <= 0 or partition is None:
+            partition = 1
+        
         last_id = 0
         while True:
-            df = self._read_data_part(last_id)
+            df = self._read_data_part(last_id, db_uri, query, order_by_column, limit, partition)
             if df is None:
                 break
 
-            last_id = df[self.order_by_column].max()
+            last_id = df[order_by_column].max()
             yield df
 
     def _generate_range(self, min_id:int, max_id:int, partition:int) -> Iterable[tuple[int, int]]:
@@ -40,14 +32,14 @@ class DeltaParquetManager:
             end_id = max_id if i == partition - 1 else start_id + bucket_size - 1
             yield start_id, end_id
 
-    def _read_data_part(self, last_id:int) -> pl.DataFrame:        
+    def _read_data_part(self, last_id:int, db_uri:str, query:str, order_by_column:str, limit:int, partition:int) -> pl.DataFrame:        
         range_query = f"""           
-            WITH Part AS (SELECT TOP {self.limit} {self.order_by_column} FROM ({self.query}) source WHERE {self.order_by_column} > {last_id} ORDER BY 1)
-            SELECT MIN({self.order_by_column}) as min_id, MAX({self.order_by_column}) as max_id FROM Part
+            WITH Part AS (SELECT TOP {limit} {order_by_column} FROM ({query}) source WHERE {order_by_column} > {last_id} ORDER BY 1)
+            SELECT MIN({order_by_column}) as min_id, MAX({order_by_column}) as max_id FROM Part
         """
         self.logger.info(f"range_query: {range_query}")
 
-        df = pl.read_database_uri(range_query, self.db_uri)
+        df = pl.read_database_uri(range_query, db_uri)
         if df.is_empty():
             return None
 
@@ -55,12 +47,10 @@ class DeltaParquetManager:
         max_id = df["max_id"][0]        
 
         part_list: list[str] = [f"""
-            SELECT * FROM ({self.query}) source where {self.order_by_column} between {start_id} and {end_id}
-            """ for start_id, end_id in self._generate_range(min_id, max_id, self.partition)]
-        
-        self.logger.info(f"part_list: {part_list}")
-        
-        df = pl.read_database_uri(part_list, self.db_uri)
+            SELECT * FROM ({query}) source where {order_by_column} between {start_id} and {end_id}
+            """ for start_id, end_id in self._generate_range(min_id, max_id, partition)]
+                
+        df = pl.read_database_uri(part_list, db_uri)
         if df.is_empty():
             return None
 
@@ -68,14 +58,12 @@ class DeltaParquetManager:
             
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    manager = DeltaParquetManager(
-        db_uri="mssql://testoltp/Store7?driver=ODBC+Driver+17+for+SQL+Server&TrustServerCertificate=yes&trusted_connection=true",
-        out_dir="veri_ambari",
-        query="SELECT * FROM tb_UrunRecete",
-        pk_columns="ID",
-        order_by_column="ID",
-        limit=100_000
-    )
+    manager = DeltaParquetManager()
+    db_reader = manager.read_database_part(
+        db_uri="mssql://testoltp/Store7?driver=ODBC+Driver+17+for+SQL+Server&TrustServerCertificate=yes&trusted_connection=true", 
+        out_dir="urunrecete", 
+        query="SELECT * FROM tb_UrunRecete", 
+        order_by_column="ID")
 
-    for df in manager.run():
+    for df in db_reader:
         print(df.describe())
