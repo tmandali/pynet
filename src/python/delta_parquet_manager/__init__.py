e6a16e74d782ae84954263018584ec9c74c9c7dd
timur-mandali_lcw <timur.mandali@lcwaikikiki.com>
2025-11-27 11:38:15 +0300

    refactor: Update DeltaParquetManager to enhance database reading logic and reinitialization
    - Added support for reinitializing the Delta table by moving the existing table to a timestamped backup if specified.
    - Improved the SQL query construction in `_read_database_mssql` to handle the `last_id` condition more flexibly.
    - Updated the `read_database_part` function to include a `reinit` parameter for better control over data processing.
    - Commented out the historical data reading section for future implementation, maintaining clarity in the main execution flow.

--- src/python/delta_parquet_manager/__init__.py
+++ src/python/delta_parquet_manager/__init__.py
@@ -1,4 +1,7 @@
+import datetime
 import logging
+import os
+import shutil
 from typing import Any, Callable
 from urllib.parse import urlparse
 import polars as pl
@@ -10,9 +13,14 @@ if not logger.handlers:
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
 
-def _read_database_mssql(db_uri:str, query:str, order_by_column:str, last_id:Any, limit:int, partition:int) -> pl.DataFrame:      
+def _read_database_mssql(db_uri:str, query:str, order_by_column:str, last_id:Any, limit:int, partition:int) -> pl.DataFrame:   
+
+    resume = ""
+    if last_id is not None:
+        resume = f"WHERE {order_by_column} > '{last_id}'"
+
     range_query = f"""           
-        WITH Part AS (SELECT TOP {limit} {order_by_column} FROM ({query}) source WHERE {order_by_column} > '{last_id}' ORDER BY 1),
+        WITH Part AS (SELECT TOP {limit} {order_by_column} FROM ({query}) source {resume} ORDER BY 1),
         Range AS (SELECT NTILE({partition}) OVER (ORDER BY {order_by_column}) as Part, {order_by_column} FROM Part)
         SELECT Part, MIN({order_by_column}) as BeginPart, MAX({order_by_column}) as EndPart FROM Range GROUP BY Part
     """
@@ -31,10 +39,17 @@ def _read_database_mssql(db_uri:str, query:str, order_by_column:str, last_id:Any
 
     return df                
 
-def read_database_part(db_uri:str, query:str, order_by_column:str, last_id:Any = 0, limit:int=1_000_000, partition:int=8):     
+def read_database_part(db_uri:str, query:str, order_by_column:str, reinit=False, limit:int=1_000_000, partition:int=8):     
     if partition <= 0 or partition is None:
         partition = 1
     
+    last_id = None    
+    if os.path.exists(delta_table):
+        if reinit:
+            shutil.move(delta_table, delta_table + "_" + datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
+        else:
+            last_id = pl.read_delta(delta_table)[order_by_column].max()
+
     parsed_db_uri = urlparse(db_uri)
     if parsed_db_uri.scheme.lower() == "mssql":
         read_database_func = _read_database_mssql
@@ -55,32 +70,31 @@ if __name__ == "__main__":
 
     db_uri = "mssql://testoltp/Store7?driver=ODBC+Driver+17+for+SQL+Server&TrustServerCertificate=yes&trusted_connection=true"
     delta_table = "./data/urunrecete"
-
-    initial_reader = read_database_part(db_uri, "SELECT * FROM tb_UrunRecete", "ID")
+  
+    initial_reader = read_database_part(db_uri, "SELECT * FROM tb_UrunRecete", "ID", reinit=True)
     for df in initial_reader:
-        df = df.with_columns(
+        df.with_columns(
             pl.col("RowVersion").bin.encode("hex"),
             pl.lit(0, pl.Int64).alias("Hist_ID"),
             pl.lit(0, pl.Int16).alias("Hist_Islem")
         ).write_delta(
             target=delta_table, 
             mode="append")
-        logger.info(f"Initial table height: {df.height}")
 
-    delta_reader = read_database_part(db_uri, "SELECT * FROM tb_UrunRecete_Hist", "Hist_ID")
-    for df in delta_reader:
-        df = df.with_columns(
-            pl.col("RowVersion").bin.encode("hex"),
-        ).filter(
-            pl.col("Hist_ID") == pl.col("Hist_ID").max().over("ID")
-        ).write_delta(
-            target=delta_table, 
-            mode="merge",
-            delta_merge_options={
-                "predicate": "s.ID = t.ID",  
-                "source_alias": "s",         
-                "target_alias": "t",   
-            }
-        ).when_matched_update_all().when_not_matched_insert_all().execute()
+    # delta_reader = read_database_part(db_uri, "SELECT * FROM tb_UrunRecete_Hist", "Hist_ID")
+    # for df in delta_reader:
+    #     df = df.with_columns(
+    #         pl.col("RowVersion").bin.encode("hex"),
+    #     ).filter(
+    #         pl.col("Hist_ID") == pl.col("Hist_ID").max().over("ID")
+    #     ).write_delta(
+    #         target=delta_table, 
+    #         mode="merge",
+    #         delta_merge_options={
+    #             "predicate": "s.ID = t.ID",  
+    #             "source_alias": "s",         
+    #             "target_alias": "t",   
+    #         }
+    #     ).when_matched_update_all().when_not_matched_insert_all().execute()
         
-        logger.info(f"Delta table height: {df.height}")
+    #     logger.info(f"Delta table height: {df.height}")
